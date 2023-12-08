37432d2241d10fa86e5f3d3911771a3d7facf4f4
Joel Strong <jdstrongpdx@gmail.com>
2023-12-07 20:59:23 -0800

    Added app.js and app.css code

--- src/App.js
+++ src/App.js
@@ -21,15 +21,78 @@ import EditHomePageForm from './components/RentalHomes/EditHomePageForm.js';
 
 
 function App() {
+
+  const [home, setHomeToEdit] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my App Version 2!
-        </p>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      
+          <header>
+            <h1><img src="./logo192.png" className="App-logo" alt="Website Logo"></img> &emsp; Joel Strong </h1>
+          </header>
+
+          <NavBar />
+          <div className='sidebarRight'></div>
+
+          <div className="content">
+          <main>
+
+            <section>
+                <Routes> 
+
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/GalleryPage" element={<GalleryPage/>}/>
+                    <Route path="/OrderPage" element={<OrderPage/>}/>
+                    <Route path="/StaffPage" element={<StaffPage/>}/>
+                    <Route path="/ContactPage" element={<Contact/>}/>
+                    <Route path="/IdealConditionsPage" element={<IdealConditionsPage/>}/>
+                    <Route path="/ResumePage" element={<ResumePage/>}/>
+
+                    <Route path="/RentalHomesPage" element={<RentalHomesPage setHome={setHomeToEdit}/>} />
+                    <Route path="/create" element={<AddHomePageForm />} /> 
+                    <Route path="/update" element={<EditHomePageForm homeToEdit={home} />} />
+
+
+                    {/*<Route path="/create" element={<AddHomePageForm />} />  */}
+                    {/*<Route path="/update" element={<EditHomePageForm homeToEdit={home} />} />*/}
+                </Routes>
+              </section>
+          </main>
+          </div>
+
+          <footer>
+            <p>
+
+            <a href="https://www.linkedin.com/in/joel-strong/" target="https://www.linkedin.com/in/joel-strong/">
+              <img height="50" alt="LinkedIn logo" src="./images/LinkedIn.png"/> 
+              </a>
+
+              &emsp;
+              <a href="https://github.com/jdstrongpdx" target="https://github.com/jdstrongpdx">
+              <img height="50" alt="GitHub logo" src="./images/GitHub_Logo.png"/> 
+              </a>
+
+              &emsp;
+              <a href="https://leetcode.com/jdstrongpdx/" target="https://leetcode.com/jdstrongpdx/">
+              <img height="50" alt="Leetcode logo" src="./images/LeetCode_logo.png"/> 
+              </a>
+
+              &emsp;
+              <a href="https://hyperskill.org/profile/404595637" target="https://hyperskill.org/profile/404595637">
+              <img height="50" alt="Hyperskill logo" src="./images/Hyperskill.jpg"/> 
+              </a>
+
+              &emsp;
+              &copy; 2023 Joel Strong
+
+            </p>
+            
+          </footer>
+
+      </BrowserRouter>
+
+    </>
   );
 }
 
