65275c01298574395792e242e1f0246f07bbf725
Joel Strong <jdstrongpdx@gmail.com>
2023-12-07 23:20:45 -0800

    Changed styling and format - still needs more work...

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Import Components, styles, media
 import NavBar from './components/NavBar.js';
-
-import logo from './logo.svg';
 import './App.css';
 
 import HomePage from './pages/HomePage.js';
@@ -27,20 +25,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      
-          <header>
-            <h1><img src="./logo192.png" className="App-logo" alt="Website Logo"></img> &emsp; Joel Strong </h1>
-          </header>
 
           <NavBar />
-          <div className='sidebarRight'></div>
 
           <div className="content">
           <main>
-
+            <h1>THIS WEBSITE IS UNDER DEVELOPMENT</h1>
             <section>
                 <Routes> 
-
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/GalleryPage" element={<GalleryPage/>}/>
                     <Route path="/OrderPage" element={<OrderPage/>}/>
@@ -60,35 +52,6 @@ function App() {
               </section>
           </main>
           </div>
-
-          <footer>
-            <p>
-
-            <a href="https://www.linkedin.com/in/joel-strong/" target="https://www.linkedin.com/in/joel-strong/">
-              <img height="50" alt="LinkedIn logo" src="./images/LinkedIn.png"/> 
-              </a>
-
-              &emsp;
-              <a href="https://github.com/jdstrongpdx" target="https://github.com/jdstrongpdx">
-              <img height="50" alt="GitHub logo" src="./images/GitHub_Logo.png"/> 
-              </a>
-
-              &emsp;
-              <a href="https://leetcode.com/jdstrongpdx/" target="https://leetcode.com/jdstrongpdx/">
-              <img height="50" alt="Leetcode logo" src="./images/LeetCode_logo.png"/> 
-              </a>
-
-              &emsp;
-              <a href="https://hyperskill.org/profile/404595637" target="https://hyperskill.org/profile/404595637">
-              <img height="50" alt="Hyperskill logo" src="./images/Hyperskill.jpg"/> 
-              </a>
-
-              &emsp;
-              &copy; 2023 Joel Strong
-
-            </p>
-            
-          </footer>
 
       </BrowserRouter>
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,52 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav class="sidebar">
-        <Link to="/">Home</Link>
+    <>
+    <div className='sidebar'>
+    <img src="./logo192.png" className='certificate' alt="Website Logo"></img>
+    <h1>Joel Strong </h1>
+
+    <p>Hi, my name is Joel Strong and I am aspiring Full Stack Software Engineer.  Weclome to my personal website!</p>
+
+
+    <nav>
+        <Link to="/">About Me</Link>
+        <p>Portfolio</p>
         <Link to="/ResumePage">Resume</Link>
         <Link to="/GalleryPage">Gallery</Link>
+        <Link to="/ContactPage">Contact</Link>
 
         <Link to="/IdealConditionsPage">IdealConditions</Link>
         <Link to="/RentalHomesPage">Rental Homes</Link>
     </nav>
+
+    <p>
+
+      <a href="https://www.linkedin.com/in/joel-strong/" target="https://www.linkedin.com/in/joel-strong/">
+        <img height="50" alt="LinkedIn logo" src="./images/LinkedIn.png"/> 
+        </a>
+
+        &emsp;
+        <a href="https://github.com/jdstrongpdx" target="https://github.com/jdstrongpdx">
+        <img height="50" alt="GitHub logo" src="./images/GitHub_Logo.png"/> 
+        </a>
+
+        &emsp;
+        <a href="https://leetcode.com/jdstrongpdx/" target="https://leetcode.com/jdstrongpdx/">
+        <img height="50" alt="Leetcode logo" src="./images/LeetCode_logo.png"/> 
+        </a>
+
+        &emsp;
+        <a href="https://hyperskill.org/profile/404595637" target="https://hyperskill.org/profile/404595637">
+        <img height="50" alt="Hyperskill logo" src="./images/Hyperskill.jpg"/> 
+        </a>
+
+        &emsp;
+        &copy; 2023 Joel Strong
+
+      </p>
+    </div>
+    </>
   );
 }
 
