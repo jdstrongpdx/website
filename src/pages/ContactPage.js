86581f8378023fdc9c6779eb781d3aba7f54aaa5
Joel Strong <jdstrongpdx@gmail.com>
2024-09-12 20:09:21 -0700

    Updated ContactPage.js and resume.

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -14,16 +14,29 @@ class Contact extends Component {
         return(
             <>
 
-            <h1>Contact Page</h1>
+                <h1>Contact Page</h1>
 
-            <p className='centered'>Welcome to my contact page!</p>
 
-            <p className='centered'>Click the following link to send me an email at <a href="mailto:jdstrongpdx@gmail.com">jdstrongpdx@gmail.com</a>.</p>
+                <div className='card'>
+                    <p className='centered'>Welcome to my contact page!</p>
 
-            <p className='centered'>I look forward to hearing from you!</p>
-            
-            
-          </>
+                    <p className='centered'>Send me an email at<a
+                        href="mailto:jdstrongpdx@gmail.com">jdstrongpdx@gmail.com</a> or view my other contact options below.</p>
+
+                    <p className='centered'>I look forward to hearing from you!</p>
+
+{/*                    <img src="/images/headshot.jpg" style={{borderRadius: '25px', paddingRight: '20 px'}} width='250px'
+                         alt="Website Logo"></img>
+                    <img src="/images/WebsiteQR.png" style={{marginLeft: '25 px'}} width='250px'
+                         alt="Website Logo"></img>*/}
+                    <h1>Joel Strong</h1>
+                    <h2>Full Stack Software Engineer</h2>
+                    <div>
+                        <img src="/images/SpringBusinessCard.png" alt="Programming Business Card"></img>
+                    </div>
+                </div>
+
+            </>
         );
     }
 }
